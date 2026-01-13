68563aa9552a458696bfb08bef14c2034ae3ef72
M4rcosBrun0 <marcosbruur@gmail.com>
2026-01-13 12:29:23 -0300

    cards de proyectos mejoradas

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,28 +6,40 @@ type ProjectCardProps = {
 
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
-    <>
-      <div className="lg:grid lg:grid-cols-5 gap-10 lg:gap-5 border-y-2 lg:border-2 border-gray-200 lg:rounded-lg lg:p-3 lg:h-80 lg:items-center overflow-hidden mb-20 lg:mb-2 pb-5 lg:py-3">
-        <div className="lg:col-span-3 lg:h-full overflow-hidden mb-2 h-1/2">
-          <img
-            src={`${project.img}.png`}
-            alt={`Imagen de ${project.title}`}
-            className="w-full h-full object-top-left rounded-lg scale-95 hover:scale-100 transition-transform duration-300 ease-in-out"
-          />
-        </div>
+    <div className="flex flex-col border-y-2 lg:border-2 border-gray-200 lg:rounded-lg lg:p-4 mb-20 lg:mb-2">
 
-        <div className="col-start-4 col-span-2 ">
-          <h2 className="text-blue-300 uppercase font-bold text-xl text-start">
-            {project.title}
-          </h2>
-          <p className="text-start">{project.desc}</p>
-          <a href={`${project.url}`} target="_blank" rel="noopener noreferrer">
-            <button className="text-center p-2 mt-4 bg-blue-700 text-white w-full hover:bg-blue-800 uppercase font-bold shadow-lg hover:cursor-pointer">
-              Entrar
-            </button>
-          </a>
-        </div>
+      {/* Imagen */}
+      <div className="w-full h-56 overflow-hidden flex items-center justify-center mb-3">
+        <img
+          src={`${project.img}.png`}
+          alt={`Imagen de ${project.title}`}
+          className="w-full h-full object-cover rounded-lg transition-transform duration-300 hover:scale-105"
+        />
       </div>
-    </>
+
+      {/* Contenido */}
+      <div className="flex flex-col flex-1">
+        <h2 className="text-blue-300 uppercase font-bold text-xl mb-2">
+          {project.title}
+        </h2>
+
+        <p className="text-sm mb-4">
+          {project.desc}
+        </p>
+
+        <a
+          href={project.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-auto"
+        >
+          <button className="p-2 bg-blue-700 text-white w-full hover:bg-blue-800 uppercase font-bold shadow-lg">
+            Entrar
+          </button>
+        </a>
+      </div>
+
+    </div>
   );
 }
+
